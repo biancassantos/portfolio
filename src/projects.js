1a20c68c1b6560883c583a68a084e364aaef20c4
Bianca Santos <biancassantos89@gmail.com>
2024-09-20 13:31:52 -0300

    removed warms project

--- src/projects.js
+++ src/projects.js
@@ -6,12 +6,6 @@ const projects = [
     imgLink: 'https://biancassantos.github.io/evergreen-interiors/',
     projectLink: 'https://github.com/biancassantos/evergreen-interiors'
   }, {
-    id: 'warms',
-    name: 'warms',
-    img: 'images/warms.jpg',
-    imgLink: 'https://biancassantos.github.io/warms/',
-    projectLink: 'https://github.com/biancassantos/warms'
-  },{
     id: 'triton',
     name: 'Triton Aquarium',
     img: 'images/triton-aquarium.jpg',
