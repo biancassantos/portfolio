f2abf878d1da52cc739d3b5ff1620f3c8c78f88f
Bianca Santos <biancassantos89@gmail.com>
2025-07-15 18:33:39 -0300

    initial commit

---
+++ src/data/projects.js
@@ -0,0 +1,39 @@
+const projects = [
+  {
+    id: "everybook",
+    name: "EveryBook",
+    img: "imgs/everybook.jpg",
+    deploy: "https://everybook.vercel.app/",
+    repository: "https://github.com/biancassantos/everybook"
+  },
+  {
+    id: "space-tourism",
+    name: "Space Tourism Website",
+    img: "imgs/space-tourism.jpg",
+    deploy: "https://biancassantos.github.io/space-tourism-website",
+    repository: "https://github.com/biancassantos/space-tourism-website"
+  },
+  {
+    id: "logdays",
+    name: "logdays",
+    img: "imgs/logdays.jpg",
+    deploy: "https://biancassantos.github.io/logdays",
+    repository: "https://github.com/biancassantos/logdays"
+  },
+  {
+    id: "pethelp",
+    name: "Pethelp",
+    img: "imgs/pethelp.jpg",
+    deploy: "https://biancassantos.github.io/pethelp/",
+    repository: "https://github.com/biancassantos/pethelp"
+  },
+  {
+    id: "evergreen-interiors",
+    name: "Evergreen Interiors",
+    img: "imgs/evergreen-interiors.jpg",
+    deploy: "https://biancassantos.github.io/evergreen-interiors/",
+    repository: "https://github.com/biancassantos/evergreen-interiors"
+  }
+]
+
+export default projects
---
+++ src/data/text.js
@@ -0,0 +1,48 @@
+const text = [
+  {
+    hero: {
+      greeting: "Olá, eu sou a Bianca",
+      stack: "Desenvolvedora Front-end",
+      button: "Conhecer"
+    },
+    about: "Sobre mim",
+    about_body: "",
+    skills: "Habilidades",
+    projects: "Projetos",
+    contact: "Contato",
+    form: {
+      name: "Nome",
+      message: "Mensagem",
+      submit: "Enviar"
+    },
+    thank_you: {
+      thanks: "Obrigada pela mensagem!",
+      message: "Entrarei em contato com você assim que for possível.",
+      link: "Voltar ao portfólio"
+    }
+  },
+  {
+    hero: {
+      greeting: "Hi, I'm Bianca",
+      stack: "Front-end Developer",
+      button: "Get to know"
+    },
+    about: "About me",
+    about_body: "",
+    skills: "Skills",
+    projects: "Projects",
+    contact: "Contact",
+    form: {
+      name: "Name",
+      message: "Message",
+      submit: "Send"
+    },
+    thank_you: {
+      thanks: "Thank you for the message!",
+      message: "I will get back to you as soon as I can.",
+      link: "Go back to the portfolio"
+    }
+  }
+]
+
+export default text
