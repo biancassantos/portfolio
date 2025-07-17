20fb9699c20677bbd17db7bcef9bb1a4c5e03c52
Bianca Santos <biancassantos89@gmail.com>
2025-07-17 10:01:34 -0300

    style: add justify-center to about me section

--- src/components/about-me/AboutMe.jsx
+++ src/components/about-me/AboutMe.jsx
@@ -9,7 +9,7 @@ function AboutMe({ mainContentRef }) {
   return (
     <section
       ref={mainContentRef}
-      className="flex h-screen flex-col items-center gap-10 px-8 py-12"
+      className="flex h-screen flex-col items-center justify-center gap-10 px-8 py-12"
     >
       <Heading text={content.about} />
 
