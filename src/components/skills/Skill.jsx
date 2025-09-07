8f408c80e75bded45e9e31e5c49f49aefeac6af6
Bianca Santos <biancassantos89@gmail.com>
2025-09-07 19:33:37 -0300

    refactor: add skill title directly in the image

--- src/components/skills/Skill.jsx
+++ src/components/skills/Skill.jsx
@@ -1,9 +1,7 @@
 function Skill({ icon, name }) {
   return (
     <div className="bg-glass shadow-highlight size-[60px] rounded-sm p-3">
-      <abbr title={name}>
-        <img src={icon} alt={`${name}'s icon`} />
-      </abbr>
+      <img src={icon} alt={`${name}'s icon`} title={name} />
     </div>
   )
 }
