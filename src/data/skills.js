783521c9a3fce0075b3335c413f0e24a62daa5f6
Bianca Santos <biancassantos89@gmail.com>
2026-03-27 19:16:44 -0300

    chore: update info and skills

--- src/data/skills.js
+++ src/data/skills.js
@@ -4,32 +4,72 @@ const skills = [
     icon: "icons/html.svg"
   },
   {
-    name: "CSS",
-    icon: "icons/css.svg"
-  },
-  {
     name: "JavaScript",
     icon: "icons/javascript.svg"
-  },
-  {
-    name: "React",
-    icon: "icons/react.svg"
   },
   {
     name: "TypeScript",
     icon: "icons/typescript.svg"
   },
   {
+    name: "React",
+    icon: "icons/react.svg"
+  },
+  {
+    name: "Redux",
+    icon: "icons/redux.svg"
+  },
+  {
+    name: "Next.js",
+    icon: "icons/nextjs.svg"
+  },
+  {
+    name: "CSS",
+    icon: "icons/css.svg"
+  },
+  {
     name: "Tailwindcss",
     icon: "icons/tailwindcss.svg"
+  },
+  {
+    name: "Sass",
+    icon: "icons/sass.svg"
+  },
+  {
+    name: "Bootstrap",
+    icon: "icons/bootstrap.svg"
+  },
+  {
+    name: "Java",
+    icon: "icons/java.svg"
+  },
+  {
+    name: "Spring Framework",
+    icon: "icons/spring.svg"
+  },
+  {
+    name: "Oracle DB",
+    icon: "icons/oracle.svg"
   },
   {
     name: "MySQL",
     icon: "icons/mysql.svg"
   },
   {
+    name: "PostrgresSQL",
+    icon: "icons/postgres.svg"
+  },
+  {
     name: "Git",
     icon: "icons/git.svg"
+  },
+  {
+    name: "Jest",
+    icon: "icons/jest.svg"
+  },
+  {
+    name: "JUnit",
+    icon: "icons/junit.svg"
   },
   {
     name: "Figma",
